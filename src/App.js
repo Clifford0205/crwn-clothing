bcb9aee9fa2304d7d978231d86464b37100d39ed
Clifford <clifford@twjoin.com>
2021-01-20 10:06:11 +0800

    feat(router): router引入

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import Homepage from './pages/homapage/homepage.component';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Homepage></Homepage>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
 
 export default App;
- 
