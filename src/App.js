9005021e4268a86876f688686319edfb912d2ed3
Clifford <clifford@twjoin.com>
2021-01-22 10:19:44 +0800

    feat(collection-preview): 商品頁面

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
-import Homepage from './pages/homapage/homepage.component';
+import Homepage from './pages/homepage/homepage.component';
+import Shoppage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+// const HatsPage = () => (
+//   <div>
+//     <h1>HATS PAGE</h1>
+//   </div>
+// );
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route exact path="/shop" component={Shoppage} />
+
+        {/* <Route path="/hats" component={HatsPage} /> */}
       </Switch>
     </div>
   );
