7f1a452f02423119249eccc4af3e8313e72667b0
Clifford <clifford@twjoin.com>
2021-01-22 09:18:17 +0800

    feat(menu-item.component.js): router綁定完成

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -2,19 +2,26 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, history }) => (
-  <div className={`${size} menu-item`} onClick={() => history.push()}>
+// 有了 withRouter 就能拿到router傳進來的參數 這裡的 history,match就是
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+  console.log(match);
+  return (
     <div
-      className="background-image"
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    ></div>
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      ></div>
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default withRouter(MenuItem);
