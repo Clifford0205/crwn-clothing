7bed3f455b342969b1e3692a5217999665c9f0b7
Clifford <clifford@twjoin.com>
2021-01-23 12:05:28 +0800

    feat(collection-preview.js): 收藏預覽

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -6,9 +6,13 @@ const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1>{title.toUpperCase()}</h1>
     <div className="preview">
-      {items.map(item => (
-        <div key={item.id}>{items.name}</div>
-      ))}
+      {items
+        .filter((item, index) => index < 4)
+        .map(item => (
+          <div key={item.id}>{item.name}</div>
+        ))}
     </div>
   </div>
 );
+
+export default CollectionPreview;
