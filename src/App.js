b028bf5f88b97b156c0c263460385a80cccad5eb
Clifford <clifford@twjoin.com>
2021-01-25 09:30:54 +0800

    feat(form): 登入表單

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
-import Shoppage from './pages/shop/shop.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 // const HatsPage = () => (
@@ -17,7 +18,8 @@ function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={Shoppage} />
+        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path="/signIn" component={SignInAndSignUpPage} />
 
         {/* <Route path="/hats" component={HatsPage} /> */}
       </Switch>
