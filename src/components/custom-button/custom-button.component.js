78a2cf0e816218924f909d6cf0490164e25d8415
Clifford <clifford@twjoin.com>
2022-01-29 22:34:42 +0800

    feat(custom-button): styled-component

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -1,19 +1,23 @@
-import './custom-button.styles.scss';
+// import './custom-button.styles.scss';
+
+import { CustomButtonContainer } from './custom-button.styles';
 
 const CustomButton = ({
   children,
-  isGoogleSignIn,
-  inverted,
-  ...otherProps
+  ...props
+  // isGoogleSignIn,
+  // inverted,
+  // ...otherProps
 }) => (
-  <button
-    className={`${inverted ? 'inverted' : ''} ${
-      isGoogleSignIn ? 'google-sign-in' : ''
-    } custom-button`}
-    {...otherProps}
-  >
-    {children}
-  </button>
+  // <button
+  //   className={`${inverted ? 'inverted' : ''} ${
+  //     isGoogleSignIn ? 'google-sign-in' : ''
+  //   } custom-button`}
+  //   {...otherProps}
+  // >
+  //   {children}
+  // </button>
+  <CustomButtonContainer {...props}>{children}</CustomButtonContainer>
 );
 
 export default CustomButton;
